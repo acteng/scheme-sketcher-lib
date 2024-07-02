5527b418352130d56a6f03b6817315facdfa29cc
Dustin Carlino <dabreegster@gmail.com>
2024-07-02 09:01:51 +0200

    Export subdirectories, attempt 3

--- src/lib/draw/index.ts
+++ src/lib/draw/index.ts
@@ -1,6 +1,11 @@
 export { default as BoundaryLayer } from "./BoundaryLayer.svelte";
 export { default as EditGeometryMode } from "./EditGeometryMode.svelte";
+export { default as ImageLayer } from "./image/ImageLayer.svelte";
 export { default as HoverLayer } from "./HoverLayer.svelte";
 export { default as InterventionLayer } from "./InterventionLayer.svelte";
+export { default as PolygonToolLayer } from "./polygon/PolygonToolLayer.svelte";
+export { default as RouteSnapperLayer } from "./route/RouteSnapperLayer.svelte";
+export { default as SplitRouteMode } from "./route/SplitRouteMode.svelte";
 export { default as StreetViewMode } from "./StreetViewMode.svelte";
 export { default as Toolbox } from "./Toolbox.svelte";
+export * as stores from "./stores";
