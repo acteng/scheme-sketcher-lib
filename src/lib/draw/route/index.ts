c8754a8d97ac396435db680b5c9ab3e47a6e7ec9
Dustin Carlino <dabreegster@gmail.com>
2024-07-25 15:12:34 +0100

    Export RouteControls

--- src/lib/draw/route/index.ts
+++ src/lib/draw/route/index.ts
@@ -1,2 +1,3 @@
+export { default as RouteControls } from "./RouteControls.svelte";
 export { default as RouteSnapperLayer } from "./RouteSnapperLayer.svelte";
 export { default as RouteSnapperLoader } from "./RouteSnapperLoader.svelte";
