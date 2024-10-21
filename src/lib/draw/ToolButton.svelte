dcc09a29370613b8a06fb30ce1ef8ed619db93b7
Dustin Carlino <dabreegster@gmail.com>
2024-10-21 15:33:29 +0100

    Retain the toolbar at all times, and display per-mode controls beneath it

---
+++ src/lib/draw/ToolButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { IconButton } from "govuk-svelte";
+  import { mode } from "./stores";
+  import type { Mode } from "./types";
+
+  export let setMode: Mode;
+  export let disabled = false;
+</script>
+
+<IconButton
+  on:click={() => mode.set(setMode)}
+  disabled={disabled || $mode.mode != "list"}
+>
+  {#if $mode == setMode}
+    <b><slot /></b>
+  {:else}
+    <slot />
+  {/if}
+</IconButton>
