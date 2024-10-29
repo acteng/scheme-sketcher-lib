5581050f50a01117b15f65da1f88ddfddbe74b12
Dustin Carlino <dabreegster@gmail.com>
2024-10-29 19:57:16 +0000

    Redesign scheme management

--- src/routes/ExampleSchemeForm.svelte
+++ src/routes/ExampleSchemeForm.svelte
@@ -1,34 +1,15 @@
 <script lang="ts">
-  import { Modal } from "$lib/common";
-  import { DefaultButton, SecondaryButton, TextInput } from "govuk-svelte";
+  import { TextInput } from "govuk-svelte";
   import type { Schemes } from "$lib/draw/types";
   import type { Writable } from "svelte/store";
   import type { ExampleFeature, ExampleScheme } from "./types";
 
   export let gjSchemes: Writable<Schemes<ExampleFeature, ExampleScheme>>;
   export let scheme_reference: string;
-
-  let showModal = false;
-
-  function onKeyDown(e: KeyboardEvent) {
-    if (showModal && e.key == "Escape") {
-      e.stopPropagation();
-      showModal = false;
-    }
-  }
 </script>
 
-<svelte:window on:keydown={onKeyDown} />
-
-<SecondaryButton on:click={() => (showModal = true)}>
-  Scheme details
-</SecondaryButton>
-<Modal title="Scheme details" bind:open={showModal}>
-  <TextInput
-    label="Scheme name"
-    required
-    bind:value={$gjSchemes.schemes[scheme_reference].scheme_name}
-  />
-
-  <DefaultButton on:click={() => (showModal = false)}>Save</DefaultButton>
-</Modal>
+<TextInput
+  label="Scheme name"
+  required
+  bind:value={$gjSchemes.schemes[scheme_reference].scheme_name}
+/>
