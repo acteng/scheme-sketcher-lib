7a1ea45d336069e9e1216441e63e75072a47777c
Dustin Carlino <dabreegster@gmail.com>
2025-02-18 09:16:58 +0000

    When a route snapper file isn't decompressed by the browser, do it in JS

--- src/lib/draw/route/RouteSnapperLoader.svelte
+++ src/lib/draw/route/RouteSnapperLoader.svelte
@@ -4,6 +4,7 @@
   import { emptyGeojson } from "$lib/maplibre";
   import { ErrorMessage } from "govuk-svelte";
   import type { Map } from "maplibre-gl";
+  import { inflate } from "pako";
   import { init, RouteTool } from "route-snapper-ts";
   import { onMount } from "svelte";
   import { writable } from "svelte/store";
@@ -24,16 +25,8 @@
       const graphBytes = await fetchWithProgress(url, (p) => {
         progress = p;
       });
-      // The stores are unused; the WASM API is used directly. This TS wrapper is unused.
-      routeTool.set(
-        new RouteTool(
-          map,
-          graphBytes,
-          writable(emptyGeojson()),
-          writable(true),
-          writable(0),
-        ),
-      );
+
+      routeTool.set(setupRouteTool(graphBytes));
       progress = 100;
       routeToolReady = true;
     } catch (err) {
@@ -41,6 +34,35 @@
       failedToLoadRouteTool = true;
     }
   });
+
+  function setupRouteTool(graphBytes: Uint8Array): RouteTool {
+    // First trust that the browser received the "Content-Encoding: gzip" header and try to deserialize that.
+    try {
+      // The stores are unused; the WASM API is used directly. This TS wrapper is unused.
+      return new RouteTool(
+        map,
+        graphBytes,
+        writable(emptyGeojson()),
+        writable(true),
+        writable(0),
+      );
+    } catch (err) {
+      console.log(
+        `Failed to set the route snapper up (${err}). Trying to decompress first.`,
+      );
+      let inflatedBytes = inflate(graphBytes);
+      console.log(
+        `Decompression worked, trying to set up the route snapper again`,
+      );
+      return new RouteTool(
+        map,
+        inflatedBytes,
+        writable(emptyGeojson()),
+        writable(true),
+        writable(0),
+      );
+    }
+  }
 </script>
 
 {#if !routeToolReady && !failedToLoadRouteTool && !downloadComplete}
